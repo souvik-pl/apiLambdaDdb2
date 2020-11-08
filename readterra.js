00350fba5d2a9a7da1fb9a3e408a05ecee223970
Souvik <souvik.9401@gmail.com>
2020-11-08 11:15:01 +0530

    initial commit

---
+++ readterra.js
@@ -0,0 +1,44 @@
+var AWS = require('aws-sdk');
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+exports.handler = async (event) => {
+    try {
+
+            var obj = JSON.parse(event.body);
+
+			var ID = obj.id;
+        
+        
+			var params = {
+				TableName:'myTable',
+				Key: {
+					id : {S: ID}
+				}
+            
+			};
+        
+			var data;
+            
+			try{
+				data = await ddb.getItem(params).promise();
+				console.log("Item read successfully:", data);
+				} catch(err){
+					console.log("Error: ", err);
+					data = err;
+				}
+        
+        
+			var response = {
+				'statusCode': 200,
+				'body': JSON.stringify({
+					message: data
+				})
+			};
+            
+		} catch (err) {
+			console.log(err);
+			return err;
+		}
+
+    return response;
+};
